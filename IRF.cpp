1e8b3e27fe64306dd25716ef472652912870c7cb
swang1112 <59112676+swang1112@users.noreply.github.com>
2020-10-28 18:47:30 +0100

    Update IRF.cpp

--- IRF.cpp
+++ IRF.cpp
@@ -24,29 +24,29 @@ arma::mat matexp(arma::mat X, int n)
 
 // IRFs
 // [[Rcpp::export]]
-List IRF_fast(arma::mat A_hat, arma::mat B_hat, int horizon)
+List IRF_fast(arma::mat &A_hat, arma::mat &B_hat, int &horizon)
 {
-  int K = A_hat.n_rows;
+ int K = A_hat.n_rows;
   int p = A_hat.n_cols / K;
   List Out(horizon);
   Out[0] = B_hat;
   if (p == 1)
   {
-    for(int i = 1; i < horizon; i ++)
+    for(int i = 1; i < horizon; i++)
     {
       Out[i] = matexp(A_hat, i) * B_hat;
     }
     return Out;
   } else
   {
-    arma::mat Mm(K*p, K*p, arma::fill::zeros);
-    Mm.submat(0, 0, (K-1), (K*p-1)) = A_hat;
-    Mm.submat(K, 0, (K*p-1), ((p-1)*K-1)) = arma::eye(K*(p-1), K*(p-1));
-    arma::mat Mm1(K*p, K*p, arma::fill::eye);
-    for(int i = 0; i < (horizon-1); i ++)
+    arma::mat Mm(K * p, K * p, arma::fill::zeros);
+    Mm.submat(0, 0, (K - 1), (K * p - 1)) = A_hat;
+    Mm.submat(K, 0, (K * p - 1), ((p - 1) * K - 1)) = arma::eye(K * (p - 1), K * (p - 1));
+    arma::mat Mm1(K * p, K * p, arma::fill::eye);
+    for (int i = 0; i < (horizon - 1); i++)
     {
       Mm1 = Mm1 * Mm;
-      Out[i+1] = Mm1.submat(0, 0, (K-1), (K-1)) * B_hat;
+      Out[i + 1] = Mm1.submat(0, 0, (K - 1), (K - 1)) * B_hat;
     }
     return Out;
   }
